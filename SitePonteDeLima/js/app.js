6354d794701b803b523e87bf11a512a904904e86
R1chardfp <Ric.pereira2004@gmail.com>
2023-04-06 18:47:02 +0100

    4

--- SitePonteDeLima/js/app.js
+++ SitePonteDeLima/js/app.js
@@ -4,6 +4,8 @@ function Slide() {
     const mob = document.querySelector('.mob');
     const naveg = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
+    const footer = document.querySelectorAll('.foo2');
+    const video = document.querySelector('.container-video');
     //ativar
     mob.addEventListener('click', () => {
 
@@ -13,18 +15,41 @@ function Slide() {
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
+                enableScrolling();
+                
             } else {
+                disableScrolling();
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+                //document.getElementsByClassName('foo2')[0].style.visibility = 'hidden';
             }
+            
+        });
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                document.getElementsByClassName('container-video')[0].style.visibility = 'hidden';
 
+                document.getElementsByClassName('foo2')[0].style.visibility = 'hidden';
+            } else {
+                document.getElementsByClassName('container-video')[0].style.visibility = 'visible';
+                document.getElementsByClassName('foo2')[0].style.visibility = 'visible';
+            }
+            
         });
         //mob animation
         mob.classList.toggle('toggle');
-
+        
     });
 
 }
 
+function disableScrolling(){
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);};
+}
 
+function enableScrolling(){
+    window.onscroll=function(){};
+}
 
 Slide();
